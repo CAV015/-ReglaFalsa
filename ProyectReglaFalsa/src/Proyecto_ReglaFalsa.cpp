b08ee0f90a1fef94c785c165610cf79de23df2e1
CAV015 <133435023+CAV015@users.noreply.github.com>
2023-06-18 10:11:10 -0600

    Add files via upload

---
+++ ProyectReglaFalsa/src/Proyecto_ReglaFalsa.cpp
@@ -0,0 +1,29 @@
+#include "ReglaFalsa.h"
+#include "colors.h"
+
+void ejecutarReglaFalsa()
+{
+    cout << setprecision(PRECISION);
+    cout << "\nMETODO REGLA FALSA\n";
+    cout << "\nIngrese el intervalo inicial para tabular [a,b]:" << endl;
+
+    // Entrada del intervalo de tabulacion y tolerancia
+    double a, b, tolerancia = 0.00000000;
+    cout << "\na = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+
+    ReglaFalsa metodo(a, b, tolerancia);
+    metodo.tabula();
+    metodo.calcularRaiz();
+
+    cin.get();
+    cin.get();
+}
+
+int main()
+{
+    ejecutarReglaFalsa();
+    return 0;
+}
